5e3db78995836ac1c12dbcbab8e90543ce92f546
Timothy Cheung <timothyltcheung@gmail.com>
2019-01-27 13:56:22 -0500

    Reorganized code for clarity and added comments

--- static/index.js
+++ static/index.js
@@ -7,42 +7,30 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#displayName').innerHTML = "Display Name = " + localStorage.getItem('displayName');
 });
 
+
+// Function to keep messsage window scrolled to most recent message at bottom
 function updateScroll(){
     var element = document.getElementById("messagebox");
     element.scrollTop = element.scrollHeight;
     return false;
 };
 
-// Access global variable to load updated channel list
-document.addEventListener('DOMContentLoaded', function () {
-  const request = new XMLHttpRequest();
-  request.open("GET", "/channel_list");
-  request.send();
-  request.onload = () => {
-    var data = JSON.parse(request.responseText);
-    Object.keys(data).forEach(function(channel) {
-      var new_li = document.createElement('li');
-      new_li.innerHTML = '<pre><a href="javascript:void(0);">' + channel + '</a> </pre>';
-      new_li.setAttribute("onClick", "changeChannel('" + encodeURIComponent(channel) + "');");
-      new_li.className = "list-group-item";
-      new_li.id = channel;
-      document.querySelector('#channel_list').append(new_li);
-    });
-    // Need to put this function inside request because of async
-    if(localStorage.getItem('lastchannel')) {
-      changeChannel(localStorage.getItem('lastchannel'));
-    }
-  };
-});
 
+// Change channel and load new messages
 function changeChannel(channel) {
+  // decode any escaped strings back to original string
   channel = decodeURIComponent(channel)
+
+  // change active channel to light blue coloring
   document.querySelector('#messages').innerHTML = '';
   listelements=document.querySelectorAll('.list-group-item');
   listelements.forEach(function(listelement) {
     listelement.style.backgroundColor = "white";
   });
   document.getElementById(channel).style.backgroundColor = "lightblue";
+
+  // AJAX request to get channel messages from Flask server
+  // and populate the message window
   const request = new XMLHttpRequest();
   request.open("GET", "/channel_list");
   request.send();
@@ -55,12 +43,39 @@ function changeChannel(channel) {
     });
     updateScroll();
   };
+  // Activate chat input box when a channel is active and save last channel visited
   document.querySelector("#newmessage_form").style.visibility = "visible";
   localStorage.setItem('lastchannel', channel);
   return false;
 };
 
+
+// On load, access list of channels from Flask server and load channel list
+document.addEventListener('DOMContentLoaded', function () {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/channel_list");
+  request.send();
+  request.onload = () => {
+    var data = JSON.parse(request.responseText);
+    // Populate channel list with channels
+    Object.keys(data).forEach(function(channel) {
+      var new_li = document.createElement('li');
+      new_li.innerHTML = '<pre><a href="javascript:void(0);">' + channel + '</a> </pre>';
+      new_li.setAttribute("onClick", "changeChannel('" + encodeURIComponent(channel) + "');");
+      new_li.className = "list-group-item";
+      new_li.id = channel;
+      document.querySelector('#channel_list').append(new_li);
+    });
+    // If user has been to a channel previously, load last channel from localStorage
+    if(localStorage.getItem('lastchannel')) {
+      changeChannel(localStorage.getItem('lastchannel'));
+    }
+  };
+});
+
+// Connect to socket after all loading
 var socket = io.connect();
+
 
 // Adding a new channel
 document.addEventListener('DOMContentLoaded', function() {
@@ -84,10 +99,11 @@ document.addEventListener('DOMContentLoaded', function() {
     request.onload = () => {
       var data = JSON.parse(request.responseText);
       if (!Object.keys(data).includes(new_channel)) {
-        // update Flask global channel list variable using websocket
+      // update Flask global channel list variable using websocket
           socket.emit('new channel', {'new_channel': new_channel});
       }
       else {
+      // if channel name already used, send error alert
       alert('Channel already exists');
       }
     };
@@ -96,7 +112,8 @@ document.addEventListener('DOMContentLoaded', function() {
   };
 });
 
-// update side bar of channels when a new channel is added
+
+// update side bar of channels when a new channel is added for all users
 socket.on('update channel list', newchannelname => {
   var new_li = document.createElement('li');
   new_li.innerHTML = '<pre> <a href="javascript:void(0);">' + `${newchannelname.new_channel}` + '</a></pre>';
@@ -105,6 +122,7 @@ socket.on('update channel list', newchannelname => {
   new_li.id = `${newchannelname.new_channel}`;
   document.querySelector('#channel_list').append(new_li);
 });
+
 
 // Adding a new message
 document.addEventListener('DOMContentLoaded', function() {
@@ -121,14 +139,18 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#newmessage_form').onsubmit = function() {
     var new_message = document.querySelector('#newmessage').value;
     document.querySelector('#newmessage').value = '';
+    // when message is submitted, emit messsage, user and channel
     socket.emit('new message',{'new_message': new_message, "channel": localStorage.getItem('lastchannel'),
     "user": localStorage.getItem('displayName')});
     return false;
   };
 });
 
+
 // update channel messages when a new message is entered
 socket.on('update messages', newmessage => {
+  // If condition to update only if user is in the channel where the message is submitted
+  // If not, messages will be updated when the user switches to that channel via changeChannel function
   if (`${newmessage.channel}` == localStorage.getItem('lastchannel')) {
         var new_li = document.createElement('li');
         new_li.innerHTML = '<pre>' + `${newmessage.new_message.timestamp} ${newmessage.new_message.user}: ${newmessage.new_message.message}` + '</pre>';
