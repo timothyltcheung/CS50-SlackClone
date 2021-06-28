7efc27bb58b1f50271e7fa421073d0dae7d7e1a2
Tim <timothyltcheung@gmail.com>
2021-06-28 00:02:59 -0400

    Cleaned up per PEP8

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 import os
 import datetime
 
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, render_template, jsonify
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
@@ -9,18 +9,21 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
 # Maximum number of messages to store in Flask server memory per channel
-messageMax = 100
+MESSAGE_MAX = 100
 # Dictionary that will store the channel name as the key and a
 # list of messages as the value
 channels = {}
+
 
 @app.route("/")
 def index():
     return render_template('home.html', channels=channels)
 
+
 @app.route("/channel_list", methods=["GET"])
 def channelList():
     return jsonify(channels)
+
 
 @socketio.on('new channel')
 def addNewChannel(newchannel):
@@ -31,16 +34,17 @@ def addNewChannel(newchannel):
     channels[newchannelname] = []
     emit('update channel list', {"new_channel": newchannelname}, broadcast=True)
 
+
 @socketio.on('new message')
 def addNewMessage(newMessage):
     # Construct the full message with username, timestamp, and message
     fullMessage = {"user": newMessage['user'], "timestamp": str(datetime.datetime.now().strftime("%x %X")), "message": newMessage['new_message']}
     # Check if maximum number of messages stored has been reached
-    if len(channels[newMessage['channel']]) >= messageMax:
+    if len(channels[newMessage['channel']]) >= MESSAGE_MAX:
         # If max reached, append message to end of list and discard first element
         channels[newMessage['channel']].append(fullMessage)
-        channels[newMessage['channel']].pop(0);
+        channels[newMessage['channel']].pop(0)
     else:
         # Else if max not reached, append message to list
         channels[newMessage['channel']].append(fullMessage)
-    emit('update messages', {"channel": newMessage['channel'],"new_message":fullMessage}, broadcast=True)
+    emit('update messages', {"channel": newMessage['channel'], "new_message": fullMessage}, broadcast=True)
